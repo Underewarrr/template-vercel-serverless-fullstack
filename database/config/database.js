e41995110990ebe1aacd5c9d4b2dc269fc3b93fc
Underewarrr <rafhaelxd@gmail.com>
2023-06-29 11:47:18 -0300

    Fix: SIntax for js

--- database/config/database.js
+++ database/config/database.js
@@ -1,21 +1,19 @@
-import mysql2 from 'mysql2'; // Fix : problema ao lançar mysql no webpack vercel serverless
-
-
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
+const mysql2 = require('mysql2');
+
 const config = {
-    username: process.env.DB_USER || 'opentibiacourse',
-    database: process.env.DB_NAME || 'opentibiacourse',
-    password: process.env.DB_PASS || 'dbrafhaeloliveirarA@',
-    host: process.env.DB_HOST || 'db4free.net',
-    port: Number(process.env.DB_PORT) || 3306,
-    dialect: 'mysql',
-    dialectModule: mysql2, // Fix : problema ao lançar mysql no webpack vercel serverless
-    dialectOptions: {
-        timezone: 'Z',
-    },
-    logging: true,
+  username: process.env.DB_USER || '',
+  database: process.env.DB_NAME || '',
+  password: process.env.DB_PASS || '',
+  host: process.env.DB_HOST || 'db4free.net',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+  dialectModule: mysql2,
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: true,
 };
+
 module.exports = config;
-//# sourceMappingURL=database.js.map
